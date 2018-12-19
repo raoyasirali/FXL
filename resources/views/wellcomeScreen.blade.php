189fa71703f2a80131c1e82f7643385ac72c5408
Shahid <15009065217@umt.edu.pk>
2018-12-19 16:52:02 +0500

    Customer menu

--- resources/views/wellcomeScreen.blade.php
+++ resources/views/wellcomeScreen.blade.php
@@ -1,2 +1,33 @@
 @include('master')
-<div > ABC </div>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<style type="text/css">
+	#example2 {
+ 
+  padding: 25px;
+  background: url();
+  background-repeat: no-repeat;
+  background-size: 1540px 300px;
+  }
+</style>
+<div id="example2" style="height: 300px">
+  <h2><b style="margin-left: 180px">Order Online Food In Lahore</b> </h2>
+
+ <form>
+  <select >
+  <option value="Select Area">Select Area</option>
+  <option value="Township">Township</option>
+  <option value="Johartown">Johartown</option>
+  <option value="Cantt">Cantt</option>
+</select>
+<select >
+  <option value="Select Area">Cusine Type</option>
+  <option value="Township">Desi</option>
+  <option value="Johartown">Italian</option>
+  <option value="Cantt">Chinese</option>
+</select>
+<input type="submit" name="" value="Find Resturants">
+</form> 
+  
+</div>
